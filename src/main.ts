40360d2cf5d2fd4981cbbbc6a58dd8c91ea132df
I Gusti Bagus Putrawan <114229403+bgsptr@users.noreply.github.com>
2024-12-13 17:19:26 +0800

    cron receive notification

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response } from 'express';
 import { authMiddleware } from './middlewares/authMiddleware';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
@@ -7,8 +7,9 @@ import { mainRouter } from './routes/mainRoute';
 import { PushJournalNotification } from './internal/services/messaging/PushJournalNotification';
 import { ReceiveNotification } from './internal/services/messaging/ReceiveNotification';
 import nation from './handlers/nation';
-// import { predictHandler } from './handlers/predictHandler';
+import { predictHandler } from './handlers/predictHandler';
 import schedule from 'node-schedule';
+import { AuthenticatedRequest } from '../types/interfaces/interface.common';
 
 dotenv.config();
 
@@ -21,17 +22,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/api/v1', mainRouter);
-// app.post('/predict', predictHandler);
+app.post('/predict', predictHandler);
 
 const pushNotification = new PushJournalNotification();
 
 const pullNotification = new ReceiveNotification();
 
-// schedule.scheduleJob('*/2 * * * *', () => {
-//     pushNotification.producer();
-// });
+schedule.scheduleJob('*/2 * * * *', () => {
+    pushNotification.producer();
+});
 
-// app.get('/api/v1/notifications', authMiddleware, pullNotification.consumer);
+// app.post('/api/v1/notifications', authMiddleware, pullNotification.consumer);
+
+let job: schedule.Job | null = null;
+
+app.post(
+  "/api/v1/notifications",
+  authMiddleware,
+  async (req: AuthenticatedRequest, res: Response): Promise<any> => {
+    if (!req.email) return res.send("no token");
+    console.log("eksekusi cron dalam 1 menit")
+    const action = Number(req.query.action) || null;
+
+    if (action == 0) {
+      if (!job) {
+          job = schedule.scheduleJob("* * * * *", async () => {
+            try {
+              console.log("Executing pull notification cron job...");
+              await pullNotification.consumer(req, res);
+            } catch (error) {
+              console.error("Error in cron job execution:", error);
+            }
+          });
+      }
+    }
+
+    if (action == 1) {
+      if (job) {
+        job.cancel();
+        job = null;
+      }
+    }
+  }
+);
 
 
 app.get('/nations', nation);
@@ -40,5 +73,5 @@ app.get('/hello', authMiddleware, () => {
 })
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`server run on http://0.0.0.0:${port}`);
+    console.log(server run on http://0.0.0.0:${port});
 });
